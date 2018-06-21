db1d9c3296712e6dafc6c7c15a3e261f3e61e0ce
Maria Mayer <mayermaru@gmail.com>
2018-06-20 23:43:11 -0300

    Change to serve in master

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   entry: ["./src/index.js"],
   output: {
-    path: path.join(__dirname, "examples/dist"),
+    path: path.join(__dirname, "builds/dist"),
     publicPath: "/",
     filename: "bundle.js"
   },
