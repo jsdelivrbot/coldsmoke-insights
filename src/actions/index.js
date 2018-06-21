c84512c7c9451e873a56f0d4db8d839050cb0fe8
Maria Mayer <mayermaru@gmail.com>
2018-06-20 23:27:28 -0300

    change action

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,7 +6,7 @@ export const FETCH_POST = "fetch_post";
 const ROOT_URL = "https://work-place.herokuapp.com";
 
 export function fetchPosts(perPage,page,sortBy,criteria,filter) {
-  const request = axios.get(`${ROOT_URL}/api/addresses/${perPage}/${page}?sortBy=${sortBy}&criteria=${criteria}&filter=${criteria}`);
+  const request = axios.get(`${ROOT_URL}/api/addresses/${perPage}/${page}?sortBy=${sortBy}&criteria=${criteria}&filter=${filter}`);
 
   return {
     type: FETCH_POSTS,
