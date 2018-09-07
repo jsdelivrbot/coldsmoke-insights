84e6df5f160369c5dbd4b647bca0621e875d602e
Neel Bhagwat <neil.bhagwat1999@gmail.com>
2018-09-07 14:42:44 -0400

    Updates

--- bundle.js
+++ bundle.js
@@ -77978,7 +77978,11 @@
 
 	var _Menu4 = _interopRequireDefault(_Menu3);
 
+	var _reactSticky = __webpack_require__(714);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -77997,6 +78001,11 @@
 	  menuButton: {
 	    marginLeft: -12,
 	    marginRight: 20
+	  },
+	  scroll: {
+	    width: '100%',
+	    position: 'fixed',
+	    marginTop: -20
 	  }
 	};
 
@@ -78029,6 +78038,8 @@
 	  _createClass(MenuAppBar, [{
 	    key: 'render',
 	    value: function render() {
+	      var _React$createElement;
+
 	      var classes = this.props.classes;
 	      var _state = this.state,
 	          auth = _state.auth,
@@ -78047,10 +78058,10 @@
 	            { className: classes.root },
 	            _react2.default.createElement(
 	              _AppBar2.default,
-	              { className: 'bar', position: 'static' },
+	              (_React$createElement = { className: 'header' }, _defineProperty(_React$createElement, 'className', 'bar'), _defineProperty(_React$createElement, 'position', 'fixed'), _defineProperty(_React$createElement, 'className', classes.scroll), _React$createElement),
 	              _react2.default.createElement(
 	                _Toolbar2.default,
-	                null,
+	                { className: 'header' },
 	                _react2.default.createElement(
 	                  _IconButton2.default,
 	                  { className: classes.menuButton, color: 'inherit', 'aria-label': 'Menu' },
@@ -79281,6 +79292,461 @@
 	});
 
 	var _Menu = _interopRequireDefault(__webpack_require__(592));
+
+/***/ }),
+/* 714 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	exports.StickyContainer = exports.Sticky = undefined;
+
+	var _Sticky = __webpack_require__(715);
+
+	var _Sticky2 = _interopRequireDefault(_Sticky);
+
+	var _Container = __webpack_require__(716);
+
+	var _Container2 = _interopRequireDefault(_Container);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	exports.Sticky = _Sticky2.default;
+	exports.StickyContainer = _Container2.default;
+	exports.default = _Sticky2.default;
+
+/***/ }),
+/* 715 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(3);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactDom = __webpack_require__(13);
+
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+
+	var _propTypes = __webpack_require__(28);
+
+	var _propTypes2 = _interopRequireDefault(_propTypes);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Sticky = function (_Component) {
+	  _inherits(Sticky, _Component);
+
+	  function Sticky() {
+	    var _ref;
+
+	    var _temp, _this, _ret;
+
+	    _classCallCheck(this, Sticky);
+
+	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	      args[_key] = arguments[_key];
+	    }
+
+	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Sticky.__proto__ || Object.getPrototypeOf(Sticky)).call.apply(_ref, [this].concat(args))), _this), _this.state = {
+	      isSticky: false,
+	      wasSticky: false,
+	      style: {}
+	    }, _this.handleContainerEvent = function (_ref2) {
+	      var distanceFromTop = _ref2.distanceFromTop,
+	          distanceFromBottom = _ref2.distanceFromBottom,
+	          eventSource = _ref2.eventSource;
+
+	      var parent = _this.context.getParent();
+
+	      var preventingStickyStateChanges = false;
+	      if (_this.props.relative) {
+	        preventingStickyStateChanges = eventSource !== parent;
+	        distanceFromTop = -(eventSource.scrollTop + eventSource.offsetTop) + _this.placeholder.offsetTop;
+	      }
+
+	      var placeholderClientRect = _this.placeholder.getBoundingClientRect();
+	      var contentClientRect = _this.content.getBoundingClientRect();
+	      var calculatedHeight = contentClientRect.height;
+
+	      var bottomDifference = distanceFromBottom - _this.props.bottomOffset - calculatedHeight;
+
+	      var wasSticky = !!_this.state.isSticky;
+	      var isSticky = preventingStickyStateChanges ? wasSticky : distanceFromTop <= -_this.props.topOffset && distanceFromBottom > -_this.props.bottomOffset;
+
+	      distanceFromBottom = (_this.props.relative ? parent.scrollHeight - parent.scrollTop : distanceFromBottom) - calculatedHeight;
+
+	      var style = !isSticky ? {} : {
+	        position: "fixed",
+	        top: bottomDifference > 0 ? _this.props.relative ? parent.offsetTop - parent.offsetParent.scrollTop : 0 : bottomDifference,
+	        left: placeholderClientRect.left,
+	        width: placeholderClientRect.width
+	      };
+
+	      if (!_this.props.disableHardwareAcceleration) {
+	        style.transform = "translateZ(0)";
+	      }
+
+	      _this.setState({
+	        isSticky: isSticky,
+	        wasSticky: wasSticky,
+	        distanceFromTop: distanceFromTop,
+	        distanceFromBottom: distanceFromBottom,
+	        calculatedHeight: calculatedHeight,
+	        style: style
+	      });
+	    }, _temp), _possibleConstructorReturn(_this, _ret);
+	  }
+
+	  _createClass(Sticky, [{
+	    key: "componentWillMount",
+	    value: function componentWillMount() {
+	      if (!this.context.subscribe) throw new TypeError("Expected Sticky to be mounted within StickyContainer");
+
+	      this.context.subscribe(this.handleContainerEvent);
+	    }
+	  }, {
+	    key: "componentWillUnmount",
+	    value: function componentWillUnmount() {
+	      this.context.unsubscribe(this.handleContainerEvent);
+	    }
+	  }, {
+	    key: "componentDidUpdate",
+	    value: function componentDidUpdate() {
+	      this.placeholder.style.paddingBottom = this.props.disableCompensation ? 0 : (this.state.isSticky ? this.state.calculatedHeight : 0) + "px";
+	    }
+	  }, {
+	    key: "render",
+	    value: function render() {
+	      var _this2 = this;
+
+	      var element = _react2.default.cloneElement(this.props.children({
+	        isSticky: this.state.isSticky,
+	        wasSticky: this.state.wasSticky,
+	        distanceFromTop: this.state.distanceFromTop,
+	        distanceFromBottom: this.state.distanceFromBottom,
+	        calculatedHeight: this.state.calculatedHeight,
+	        style: this.state.style
+	      }), {
+	        ref: function ref(content) {
+	          _this2.content = _reactDom2.default.findDOMNode(content);
+	        }
+	      });
+
+	      return _react2.default.createElement(
+	        "div",
+	        null,
+	        _react2.default.createElement("div", { ref: function ref(placeholder) {
+	            return _this2.placeholder = placeholder;
+	          } }),
+	        element
+	      );
+	    }
+	  }]);
+
+	  return Sticky;
+	}(_react.Component);
+
+	Sticky.propTypes = {
+	  topOffset: _propTypes2.default.number,
+	  bottomOffset: _propTypes2.default.number,
+	  relative: _propTypes2.default.bool,
+	  children: _propTypes2.default.func.isRequired
+	};
+	Sticky.defaultProps = {
+	  relative: false,
+	  topOffset: 0,
+	  bottomOffset: 0,
+	  disableCompensation: false,
+	  disableHardwareAcceleration: false
+	};
+	Sticky.contextTypes = {
+	  subscribe: _propTypes2.default.func,
+	  unsubscribe: _propTypes2.default.func,
+	  getParent: _propTypes2.default.func
+	};
+	exports.default = Sticky;
+
+/***/ }),
+/* 716 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	"use strict";
+
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+
+	var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(3);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _propTypes = __webpack_require__(28);
+
+	var _propTypes2 = _interopRequireDefault(_propTypes);
+
+	var _raf = __webpack_require__(717);
+
+	var _raf2 = _interopRequireDefault(_raf);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var Container = function (_PureComponent) {
+	  _inherits(Container, _PureComponent);
+
+	  function Container() {
+	    var _ref;
+
+	    var _temp, _this, _ret;
+
+	    _classCallCheck(this, Container);
+
+	    for (var _len = arguments.length, args = Array(_len), _key = 0; _key < _len; _key++) {
+	      args[_key] = arguments[_key];
+	    }
+
+	    return _ret = (_temp = (_this = _possibleConstructorReturn(this, (_ref = Container.__proto__ || Object.getPrototypeOf(Container)).call.apply(_ref, [this].concat(args))), _this), _this.events = ["resize", "scroll", "touchstart", "touchmove", "touchend", "pageshow", "load"], _this.subscribers = [], _this.rafHandle = null, _this.subscribe = function (handler) {
+	      _this.subscribers = _this.subscribers.concat(handler);
+	    }, _this.unsubscribe = function (handler) {
+	      _this.subscribers = _this.subscribers.filter(function (current) {
+	        return current !== handler;
+	      });
+	    }, _this.notifySubscribers = function (evt) {
+	      if (!_this.framePending) {
+	        var currentTarget = evt.currentTarget;
+
+
+	        _this.rafHandle = (0, _raf2.default)(function () {
+	          _this.framePending = false;
+
+	          var _this$node$getBoundin = _this.node.getBoundingClientRect(),
+	              top = _this$node$getBoundin.top,
+	              bottom = _this$node$getBoundin.bottom;
+
+	          _this.subscribers.forEach(function (handler) {
+	            return handler({
+	              distanceFromTop: top,
+	              distanceFromBottom: bottom,
+	              eventSource: currentTarget === window ? document.body : _this.node
+	            });
+	          });
+	        });
+	        _this.framePending = true;
+	      }
+	    }, _this.getParent = function () {
+	      return _this.node;
+	    }, _temp), _possibleConstructorReturn(_this, _ret);
+	  }
+
+	  _createClass(Container, [{
+	    key: "getChildContext",
+	    value: function getChildContext() {
+	      return {
+	        subscribe: this.subscribe,
+	        unsubscribe: this.unsubscribe,
+	        getParent: this.getParent
+	      };
+	    }
+	  }, {
+	    key: "componentDidMount",
+	    value: function componentDidMount() {
+	      var _this2 = this;
+
+	      this.events.forEach(function (event) {
+	        return window.addEventListener(event, _this2.notifySubscribers);
+	      });
+	    }
+	  }, {
+	    key: "componentWillUnmount",
+	    value: function componentWillUnmount() {
+	      var _this3 = this;
+
+	      if (this.rafHandle) {
+	        _raf2.default.cancel(this.rafHandle);
+	        this.rafHandle = null;
+	      }
+
+	      this.events.forEach(function (event) {
+	        return window.removeEventListener(event, _this3.notifySubscribers);
+	      });
+	    }
+	  }, {
+	    key: "render",
+	    value: function render() {
+	      var _this4 = this;
+
+	      return _react2.default.createElement("div", _extends({}, this.props, {
+	        ref: function ref(node) {
+	          return _this4.node = node;
+	        },
+	        onScroll: this.notifySubscribers,
+	        onTouchStart: this.notifySubscribers,
+	        onTouchMove: this.notifySubscribers,
+	        onTouchEnd: this.notifySubscribers
+	      }));
+	    }
+	  }]);
+
+	  return Container;
+	}(_react.PureComponent);
+
+	Container.childContextTypes = {
+	  subscribe: _propTypes2.default.func,
+	  unsubscribe: _propTypes2.default.func,
+	  getParent: _propTypes2.default.func
+	};
+	exports.default = Container;
+
+/***/ }),
+/* 717 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(global) {var now = __webpack_require__(718)
+	  , root = typeof window === 'undefined' ? global : window
+	  , vendors = ['moz', 'webkit']
+	  , suffix = 'AnimationFrame'
+	  , raf = root['request' + suffix]
+	  , caf = root['cancel' + suffix] || root['cancelRequest' + suffix]
+
+	for(var i = 0; !raf && i < vendors.length; i++) {
+	  raf = root[vendors[i] + 'Request' + suffix]
+	  caf = root[vendors[i] + 'Cancel' + suffix]
+	      || root[vendors[i] + 'CancelRequest' + suffix]
+	}
+
+	// Some versions of FF have rAF but not cAF
+	if(!raf || !caf) {
+	  var last = 0
+	    , id = 0
+	    , queue = []
+	    , frameDuration = 1000 / 60
+
+	  raf = function(callback) {
+	    if(queue.length === 0) {
+	      var _now = now()
+	        , next = Math.max(0, frameDuration - (_now - last))
+	      last = next + _now
+	      setTimeout(function() {
+	        var cp = queue.slice(0)
+	        // Clear queue here to prevent
+	        // callbacks from appending listeners
+	        // to the current frame's queue
+	        queue.length = 0
+	        for(var i = 0; i < cp.length; i++) {
+	          if(!cp[i].cancelled) {
+	            try{
+	              cp[i].callback(last)
+	            } catch(e) {
+	              setTimeout(function() { throw e }, 0)
+	            }
+	          }
+	        }
+	      }, Math.round(next))
+	    }
+	    queue.push({
+	      handle: ++id,
+	      callback: callback,
+	      cancelled: false
+	    })
+	    return id
+	  }
+
+	  caf = function(handle) {
+	    for(var i = 0; i < queue.length; i++) {
+	      if(queue[i].handle === handle) {
+	        queue[i].cancelled = true
+	      }
+	    }
+	  }
+	}
+
+	module.exports = function(fn) {
+	  // Wrap in a new function to prevent
+	  // `cancel` potentially being assigned
+	  // to the native rAF function
+	  return raf.call(root, fn)
+	}
+	module.exports.cancel = function() {
+	  caf.apply(root, arguments)
+	}
+	module.exports.polyfill = function(object) {
+	  if (!object) {
+	    object = root;
+	  }
+	  object.requestAnimationFrame = raf
+	  object.cancelAnimationFrame = caf
+	}
+
+	/* WEBPACK VAR INJECTION */}.call(exports, (function() { return this; }())))
+
+/***/ }),
+/* 718 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	/* WEBPACK VAR INJECTION */(function(process) {// Generated by CoffeeScript 1.12.2
+	(function() {
+	  var getNanoSeconds, hrtime, loadTime, moduleLoadTime, nodeLoadTime, upTime;
+
+	  if ((typeof performance !== "undefined" && performance !== null) && performance.now) {
+	    module.exports = function() {
+	      return performance.now();
+	    };
+	  } else if ((typeof process !== "undefined" && process !== null) && process.hrtime) {
+	    module.exports = function() {
+	      return (getNanoSeconds() - nodeLoadTime) / 1e6;
+	    };
+	    hrtime = process.hrtime;
+	    getNanoSeconds = function() {
+	      var hr;
+	      hr = hrtime();
+	      return hr[0] * 1e9 + hr[1];
+	    };
+	    moduleLoadTime = getNanoSeconds();
+	    upTime = process.uptime() * 1e9;
+	    nodeLoadTime = moduleLoadTime - upTime;
+	  } else if (Date.now) {
+	    module.exports = function() {
+	      return Date.now() - loadTime;
+	    };
+	    loadTime = Date.now();
+	  } else {
+	    module.exports = function() {
+	      return new Date().getTime() - loadTime;
+	    };
+	    loadTime = new Date().getTime();
+	  }
+
+	}).call(this);
+
+	//# sourceMappingURL=performance-now.js.map
+
+	/* WEBPACK VAR INJECTION */}.call(exports, __webpack_require__(2)))
 
 /***/ })
 /******/ ]);
